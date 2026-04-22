244235cb3ecac4e6eb182566e88a2ca25c5f16dd
Justin Pacis <jpacis08@gmail.com>
2026-04-22 15:46:51 -0700

    added ArrayList, homeScreen, and gameScreen

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -5,31 +5,92 @@ import java.util.*;
 
 public class AdventureTime {
 
+    static ArrayList<StepClass> adventureSteps;
+
     static void main() {
 
-        loadAdventureTime();
+        adventureSteps = loadAdventureTime();
+        homeScreen();
 
     }
 
-    public static void loadAdventureTime(){
+    public static void homeScreen() {
 
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Welcome to Adventure Time.");
+        System.out.println("----------------------------------------");
+        System.out.println("Press (P) to play ");
+        System.out.println("Press (Q) to quit ");
+        System.out.println("===========================");
+        System.out.println("Your choice: ");
+        String choice = userInput.nextLine().toUpperCase().trim();
+        if (choice.equals("P")) {
+            gameScreen(1);
+
+        }
+    }
+
+    public static void gameScreen(int id) {
+        for (int i = 0; i < adventureSteps.size(); i++){
+            StepClass step = adventureSteps.get(i);
+            if (step.getId() == id){
+                System.out.println("Story Text: " + step.getStoryText());
+                System.out.println();
+                System.out.println("1) " + step.getOption1Text());
+                System.out.println();
+                System.out.print("2) " + step.getOption2Text());
+                System.out.println();
+
+            }
+
+        }
+    }
+
+    public static ArrayList<StepClass> loadAdventureTime(){
+
+        // create the container
+//        StepClass[] steps = new StepClass[100];
+        // ArrayLists grow as needed when you add more items
+        ArrayList<StepClass> steps = new ArrayList<>();
+
+        // populate the container
         try{
             FileReader fileReader = new FileReader("adventure1.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String line = bufferedReader.readLine();
 
+            line = bufferedReader.readLine();
 
             while (line != null){
 
-                System.out.println(line);
+                String[] columns = line.split("\\|");
+                int id = Integer.parseInt(columns[0]);
+                String storyText = columns[1];
+                String option1Text = columns[2];
+                int option1NextId = Integer.parseInt(columns[3]);
+                String option2Text = columns[4];
+                int option2NextId = Integer.parseInt(columns[5]);
+
+                // create a Step from the data in the current line
+                StepClass stepClass = new StepClass(id, storyText, option1Text,
+                        option1NextId, option2Text, option2NextId);
+
+                // add the current step to the container (ArrayList)
+                steps.add(stepClass);
+
+//                System.out.println(stepClass.getStoryText());
+
                 line = bufferedReader.readLine();
             }
             fileReader.close();
 
         }
         catch (Exception ex) {
-
+            System.err.println(ex);
         }
+        // return the container
+        return steps;
     }
 }
---
+++ src/main/java/com/pluralsight/StepClass.java
@@ -0,0 +1,62 @@
+package com.pluralsight;
+
+public class StepClass {
+
+    // Variables
+    private int id;
+    private String storyText;
+    private String option1Text;
+    private int option1NextId;
+    private String option2Text;
+    private int option2NextId;
+
+    // Constructor
+    public StepClass(int id, String storyText,  String option1Text,
+                     int option1NextId, String option2Text, int option2NextId) {
+        this.id = id;
+        this.storyText = storyText;
+        this.option1Text = option1Text;
+        this.option1NextId = option1NextId;
+        this.option2Text = option2Text;
+        this.option2NextId = option2NextId;
+
+    }
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getStoryText() {
+        return storyText;
+    }
+    public void setStoryText(String storyText) {
+        this.storyText = storyText;
+    }
+    public String getOption1Text() {
+        return option1Text;
+    }
+    public void setOption1Text(String option1Text) {
+        this.option1Text = option1Text;
+    }
+    public int getOption1NextId() {
+        return option1NextId;
+    }
+    public void setOption1NextId(int option1NextId) {
+        this.option1NextId = option1NextId;
+    }
+    public String getOption2Text() {
+        return option2Text;
+    }
+    public void setOption2Text(String option2Text) {
+        this.option2Text = option2Text;
+    }
+    public int getOption2NextId() {
+        return option2NextId;
+    }
+    public void setOption2NextId(int option2NextId) {
+        this.option2NextId = option2NextId;
+    }
+}
