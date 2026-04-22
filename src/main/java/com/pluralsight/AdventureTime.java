794bff369604c82a7c1061d0f2595f67140951dc
Justin Pacis <jpacis08@gmail.com>
2026-04-22 11:24:21 -0700

    added loadAdventureTime() to main and loaded .csv file

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -1,8 +1,35 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.*;
 
-public class Main {
+public class AdventureTime {
+
     static void main() {
 
+        loadAdventureTime();
+
+    }
+
+    public static void loadAdventureTime(){
+
+        try{
+            FileReader fileReader = new FileReader("adventure1.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+
+            while (line != null){
+
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+            fileReader.close();
+
+        }
+        catch (Exception ex) {
+
+        }
     }
 }
